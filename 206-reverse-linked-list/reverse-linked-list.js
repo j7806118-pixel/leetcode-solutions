3ee3ad861bf844a124681f21cc78acf50ffbe55f
Jishnu <j7806118@gmail.com>
2026-09-15 10:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 57.3 MB (67.21%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,13 @@
+function reverseList(head) {
+    let previous = null;
+    let current = head;
+
+    while (current !== null) {
+        const next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+
+    return previous;
+}
