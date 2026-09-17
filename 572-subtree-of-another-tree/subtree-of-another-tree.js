fd4890a5c057fce090e6c8ad3d4a328f5e6e7954
Jishnu <j7806118@gmail.com>
2026-09-17 13:03:29 +0530

    Time: 3 ms (93.61%) | Memory: 62.1 MB (64.72%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.js
@@ -0,0 +1,28 @@
+function isSubtree(root, subRoot) {
+    if(subRoot===null){
+        return true;
+    }
+    if(root===null){
+        return false;
+    }
+    if(isSameTree(root,subRoot)){
+        return true;
+    }
+    return (isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot)); 
+ } 
+ function isSameTree(p,q){
+    if(p===null && q===null){
+        return true;
+    }
+    if(p===null || q===null){
+        return false;
+    }
+    if(p.val !== q.val){
+        return false;
+    } 
+      return (
+        isSameTree(p.left, q.left) &&
+        isSameTree(p.right, q.right)
+    );
+
+ };
