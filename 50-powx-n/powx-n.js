e59f09c2de1092762e4c779279da7489f51afc12
Jishnu <j7806118@gmail.com>
2026-09-19 18:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (83.62%) - LeetSync

---
+++ 50-powx-n/powx-n.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if (n === 0) return 1;
+
+    let N = n;
+    if (N < 0) {
+        x = 1 / x;
+        N = -N;
+    }
+
+    let result = 1;
+    let currentProduct = x;
+
+    while (N > 0) {
+        if (N % 2 === 1) {
+            result *= currentProduct;
+        }
+        currentProduct *= currentProduct;
+        N = Math.floor(N / 2);
+    }
+
+    return result;
+};
